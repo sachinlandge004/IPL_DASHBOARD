208410137a7675a40a5f7635abb7f99dce518e5d
Sachin Landge <sachinlandge004@gmail.com>
2021-04-15 17:54:00 +0530

    Initial commit

---
+++ src/test/java/ipl/dashbord/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl.dashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
